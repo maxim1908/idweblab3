145e24e04548fcb7b1a51f1aa783cb621af47ab4
verejan <verejancr@gmail.com>
2020-12-24 05:40:45 -0500

    verscript2.sh

--- verscript2.sh
+++ verscript2.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
+i=0
 
-echo -n "Enter a number: "
-read num
+while [ $i -le 2 ]
+do
+echo Number: $i
+((i++))
+done
 
-if [[ $num -gt 10 ]]
-then
-echo "Number is greater than 10."
-elif [[ $num -eq 10 ]]
-then
-echo "Number is equal to 10."
-else
-echo "Number is less than 10."
-fi
+
+exit 0
