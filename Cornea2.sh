0ec958cac030c9379ce391b3ba5618564cc07d40
Cornea1 <71771561+Cornea1@users.noreply.github.com>
2020-12-24 12:58:42 +0200

    Update Cornea2.sh
    Am adaugat conditii de validare

--- Cornea2.sh
+++ Cornea2.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 function F1()
 {
-echo 'Sunt Ion si am un pisoi pe nume Tom'
+  echo 'Sunt Ion si am un pisoi pe nume Tom'
 }
-
 F1
+if (( !F1 ))
+then
+  echo 'exit 0'
+  exit 0
+else
+  echo 'exit 1'
+  exit 1
+fi
