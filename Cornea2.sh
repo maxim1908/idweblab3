4c277e0c7fb1d170ed8d10aecd5ca56b9e25c940
Cornea1 <71771561+Cornea1@users.noreply.github.com>
2020-12-24 13:09:01 +0200

    Update Cornea2.sh

--- Cornea2.sh
+++ Cornea2.sh
@@ -3,12 +3,12 @@ function F1()
 {
   echo 'Sunt Ion si am un pisoi pe nume Tom'
 }
-F1
 if (( !F1 ))
 then
-  echo 'exit 0'
+  echo 'Functia exista'
+  F1
   exit 0
 else
-  echo 'exit 1'
+  echo 'Functia nu exista'
   exit 1
 fi
